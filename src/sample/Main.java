2f6faa93f956103c3597da58db3bebdc44e3a475
GustavoDeCamargo <guustavocamargo@gmail.com>
2018-09-08 00:12:33 -0300

    4 Botoes de teste

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,10 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
-        //primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 300, 275));
-        //primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
         new Simulador().simular();
     }
 
