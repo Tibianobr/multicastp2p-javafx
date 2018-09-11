ae26ee839cbbd7f4f704299f190683db0d1b0cfb
GustavoDeCamargo <guustavocamargo@gmail.com>
2018-09-11 11:46:49 -0300

    Atualizações da tela de simulação e inserção da tela de configurações

---
+++ src/controllers/ConfigController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+
+public class ConfigController {
+
+    @FXML
+    private TextField tempoResposta;
+
+    public void iniciarSimulacao() {
+        System.out.println("OK");
+    }
+}
